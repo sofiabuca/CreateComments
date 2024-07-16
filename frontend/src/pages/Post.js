b7d12b04c98b6d8f54528205d3d29847ed8205fe
Sofia Buitrago <laurabuca98@gmail.com>
2024-07-16 14:32:13 -0600

    - Add the comment for each post

--- frontend/src/pages/Post.js
+++ frontend/src/pages/Post.js
@@ -5,6 +5,7 @@ import axios from "axios";
 function Post() {
     const [postObject, setPostObject] =useState({}); //Create at empty object
     const [comments, setComments] = useState([]); //Passing a list of comments
+    const [newComment, setNewComment] = useState("");
 
     let {id} = useParams();
 
@@ -20,6 +21,17 @@ function Post() {
         });
     }, [id]);
 
+    const addComment = () =>{
+        axios.post("http://localhost:3001/comments", {
+            commentBody: newComment,
+            PostId: id })
+            .then((response) =>{
+                const commentToAdd = {commentBody: newComment}
+                setComments([...comments, commentToAdd]);
+                setNewComment(""); //Create an empty array for the value inside
+        });
+    };
+
 
   return (
     <div className="postPage">
@@ -33,17 +45,15 @@ function Post() {
         </div>
         <div className="rightSide">
             <div className="addCommentContainer">
-                <input type='text' placeholder="Comment.." autoComplete="off" />
-                <button>Add Comment</button>
+                <input type='text' placeholder="Comment.." autoComplete="off" value={newComment} onChange={(event) => {setNewComment(event.target.value)}} />
+                <button onClick={addComment}>Add Comment</button>
             </div>
-            <div className="listOfComments"></div>
+            <div className="listOfComments">
             {comments.map((comment, key) => {
-                return <div key={key} className="comment">{comment.commentBody}</div>
+                return <div key={key} className="comment">{comment.commentBody} </div>
             })}
-
-            
+            </div>
         </div>
-     
     </div>
   );
 };
