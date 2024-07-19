a41be044f324371e2e16512c81130e6d5f62b379
Sofia Buitrago <laurabuca98@gmail.com>
2024-07-19 14:41:40 -0600

    - Create login and register pages

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react';
+import axios from "axios";
 
 function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    
+
+    const login = () =>{
+        const data = { 
+            userName: username,
+            password: password
+        }
+
+        axios.post("http://localhost:3001/auth/login",data).then((response) =>{
+            console.log(response.data);
+           
+        })
+    };
+
   return (
-    <div>
+    <div className='loginContainer'>
+        <label>Username:</label>
+        <input type="text" onChange={(event) => {setUsername(event.target.value)}}></input>
+        <label>Password:</label>
+        <input type="password" onChange={(event) => {setPassword(event.target.value)}}></input>
+        <button onClick={login}>Login</button>
       
     </div>
   )
--- frontend/src/pages/Registration.js
+++ frontend/src/pages/Registration.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import axios from "axios"; 
 
 function Registration() {
+
+// Initial values of the form
+  const initialValues = {
+    userName: "",
+    password: "",
+    
+  };
+
+  // Validate the form
+  const validationSchema = Yup.object().shape({
+    password: Yup.string().min(3).max(15).required('Password text is required'),
+    userName: Yup.string().min(3, 'Username must be at least 3 characters').max(15, 'Username must be less than 15 characters').required('Username is required'),
+  });
+
+  //Submit the username and password
+  const onSubmit = (data) =>{
+    axios.post("http://localhost:3001/auth", data).then( () =>{
+        console.log(data);
+    })
+  };
+
+
   return (
     <div>
-      
+        <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+        {() => (
+          <Form className="formContainer">
+            <label>Username: </label>
+            <ErrorMessage name="userName" component="span" />
+            <Field id="inputCreatePost" name="userName" placeholder="(Ex. John123..)" />
+
+            <label>Password: </label>
+            <ErrorMessage name="password" component="span" />
+            <Field type="password" id="inputCreatePost" name="password" placeholder="Your password..." />
+            <button type='submit'>Register</button>
+          </Form>
+        )}
+      </Formik>
     </div>
   )
 }
 
-export default Registration
+export default Registration;
